542b7c27faee128f4763886cc72325090f02a415
Фаина <stjuvi@mail.ru>
2017-08-09 08:18:07 +0300

    стилизация хедера футера блока аэростат

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-  // весь ваш код
+  
+  $("a").click(function(){
+    var elementClick = $(this).attr("href");
+    var destination = $(elementClick).offset().top;
+    $('html, body').animate({ scrollTop: destination }, 900);
+    return false;
+    });
+
+	$( ".button-cross" ).hide();
+	$( ".hamburger-menu" ).hide();
+	$( ".button-hamburger" ).click(function() {
+	$( ".hamburger-menu" ).slideToggle( "slow", function() {
+	$( ".button-hamburger" ).hide();
+	$( ".button-cross" ).show();
+	});
+	});
+
+	$( ".button-cross" ).click(function() {
+	$( ".hamburger-menu" ).slideToggle( "slow", function() {
+	$( ".button-cross" ).hide();
+	$( ".button-hamburger" ).show();
+	});
+	});
+
+
 });
